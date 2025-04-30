c058b8983f550f3d1de71244af5f347becb47562
vishal <notatallVishal@gmail.com>
2025-04-30 10:40:43 -0400

    completed (?) client

--- action_client/src/client.cpp
+++ action_client/src/client.cpp
@@ -1,3 +1,19 @@
+/*
+*
+
+Action Client Specs
+- Asks for user input about the object to track (see below).
+- Sends a goal to the action server with the specified object.
+- Prints all feedback messages received from the action server
+	during tracking.
+- Prints the result message from the action server: 
+	"Tracking Successful!" or "Tracking Failed."
+- After receiving a result message from the server, whether successful or not, 
+  the client asks for new user input for an object to track and requests that 
+  the server track the new object. 
+*
+*/
+
 #include <inttypes.h>
 #include <memory>
 #include <string>
@@ -7,25 +23,30 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 
-class MyActionClient : public rclcpp::Node
+class trackObjectClient : public rclcpp::Node
 {
 public:
-  using Move = action_client::action::TrackObject;
-  using GoalHandleMove = rclcpp_action::ClientGoalHandle<Move>;
+  using trackObject = action_client::action::trackObject;
+  using userInput = input::
+  using GoalHandleTrackObject = rclcpp_action::ClientGoalHandle<trackObject>;
 
-  explicit MyActionClient(const rclcpp::NodeOptions & node_options = rclcpp::NodeOptions())
-  : Node("client", node_options), goal_done_(false)
+  explicit trackObjectClient(const rclcpp::NodeOptions & node_options = rclcpp::NodeOptions())
+  : Node("track_object_client", node_options), goal_done_(false)
   {
-    this->client_ptr_ = rclcpp_action::create_client<TrackObject>(
+    this->client_ptr_ = rclcpp_action::create_client<trackObject>(
       this->get_node_base_interface(),
       this->get_node_graph_interface(),
       this->get_node_logging_interface(),
       this->get_node_waitables_interface(),
-      "move_robot");
+      "track_object_client");
 
+
+	subscription_ = this->create_subscription<std_msgs::msgs::String>("input_topic", 10, 
+					std::bind(&trackObjectClient::input_callback, this, _1);
+					
     this->timer_ = this->create_wall_timer(
       std::chrono::milliseconds(500),
-      std::bind(&MyActionClient::send_goal, this));
+      std::bind(&trackObjectClient::send_goal, this));
   }
 
   bool is_goal_done() const
@@ -51,7 +72,7 @@ public:
       return;
     }
 
-    auto goal_msg = Move::Goal();
+    auto goal_msg = trackObject::Goal();
 	
 
     goal_msg.time_goal = 5;
@@ -60,7 +81,7 @@ public:
 
     RCLCPP_INFO(this->get_logger(), "Sending goal");
 
-    auto send_goal_options = rclcpp_action::Client<Move>::SendGoalOptions();
+    auto send_goal_options = rclcpp_action::Client<trackObject>::SendGoalOptions();
                 
     send_goal_options.goal_response_callback =
       std::bind(&MyActionClient::goal_response_callback, this, _1);
@@ -75,11 +96,18 @@ public:
   }
 
 private:
-  rclcpp_action::Client<Move>::SharedPtr client_ptr_;
+  rclcpp_action::Client<trackObject>::SharedPtr client_ptr_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  std::string objectName;
   rclcpp::TimerBase::SharedPtr timer_;
   bool goal_done_;
+  
+  void input_callback(const std_msgs::msg::String::SharedPtr msg) {
+	RCLCPP_INFO(this->get_logger(), "Got %s\n", msg->data.c_str());
+	objectName = msg->data;
+  }
 
-  void goal_response_callback(const GoalHandleMove::SharedPtr & goal_handle)
+  void goal_response_callback(const GoalHandleTrackObject::SharedPtr & goal_handle)
   {
     if (!goal_handle) {
       RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
@@ -89,29 +117,29 @@ private:
   }
 
   void feedback_callback(
-    GoalHandleMove::SharedPtr goal_handler,
-    const std::shared_ptr<const Move::Feedback> feedback)
+    GoalHandleTrackObject::SharedPtr goal_handler,
+    const std::shared_ptr<const trackObject::Feedback> feedback)
   {
     RCLCPP_INFO(
       this->get_logger(), "Feedback received: %s", feedback->feedback.c_str());
 	  
-	 if(strcmp(feedback->feedback.c_str(), "The robot touched the wall.")){
+	 if(strcmp(feedback->feedback.c_str(), "Terminate.")){
 		auto canceler = client_ptr_->async_cancel_goal(goal_handler);
 	 }
   }
 
-  void result_callback(const GoalHandleMove::WrappedResult & result)
+  void result_callback(const GoalHandleTrackObject::WrappedResult & result)
   {
     this->goal_done_ = true;
     switch (result.code) {
       case rclcpp_action::ResultCode::SUCCEEDED:
-		RCLCPP_ERROR(this->get_logger(), "Mission Accomplished!!!!");
+		RCLCPP_ERROR(this->get_logger(), "Tracking Successful!!!!");
         break;
       case rclcpp_action::ResultCode::ABORTED:
-        RCLCPP_ERROR(this->get_logger(), "The robot touched the wall :(");
+        RCLCPP_ERROR(this->get_logger(), "Tracking Failed.");
         return;
       case rclcpp_action::ResultCode::CANCELED:
-        RCLCPP_ERROR(this->get_logger(), "Action Cancelled. The robot touched the wall.");
+        RCLCPP_ERROR(this->get_logger(), "Action Cancelled. Tracking Failed.");
         return;
       default:
         RCLCPP_ERROR(this->get_logger(), "Unknown result code");
